3e344cbe61f0c2264ee88e0f18337cf64308f24c
Jacob <jacob.simon01@gmail.com>
2020-07-05 12:38:01 -0600

    Add google login

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,18 +1,29 @@
-import * as express from 'express';
-import * as path from 'path';
+import * as express from "express";
+import * as path from "path";
+// import bodyParser from "body-parser";
 
 var app = express();
 
+app.use(express.json());
+let profileObj;
+
 // Simple endpoint that returns the current time
-app.get('/api/time', function(req, res) {
-    res.send(new Date().toISOString());
+app.get("/api/time", function (req, res) {
+  res.send(new Date().toISOString());
+});
+
+app.post("/api/auth", function (req, res) {
+  // just store in memory for now
+  profileObj = req.body.profileObj;
+  // eventually this will be some sort of user model
+  res.send(profileObj);
 });
 
 // Serve static files
-app.use('/', express.static(path.join(__dirname, '/www')));
+app.use("/", express.static(path.join(__dirname, "/www")));
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,44 +1,76 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import * as React from "react";
+import { render } from "react-dom";
+import { GoogleLogin, GoogleLoginResponse } from "react-google-login";
 
 interface AppProps {
-    name: string;
+  name: string;
 }
 
 interface AppState {
-    time: string;
+  time: string;
 }
 
 export class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      time: null,
+    };
+  }
 
-    constructor(props: AppProps) {
-        super(props);
-        this.state = {
-            time: null
-        }
-    }
-    
-    componentDidMount() {
-        this.getTime();
-        setInterval(this.getTime, 2000);
-    }
+  componentDidMount() {
+    this.getTime();
+    setTimeout(this.getTime, 2000);
+  }
 
-    render() {
-        const {name} = this.props;
-        const {time} = this.state;
-        return <><h1>{name}</h1><div>{time}</div></>;
-    }
+  responseGoogle = (response: GoogleLoginResponse) => {
+    console.log(response);
+  };
 
-    private getTime = async () => {
-        const response = await fetch('/api/time', { method: 'GET' });
-        if (response.ok) {
-            this.setState({time: await response.text()});
-        }
-    }
+  render() {
+    const { name } = this.props;
+    const { time } = this.state;
+    return (
+      <>
+        <h1>{name}</h1>
+        <div>{time}</div>
+        <GoogleLogin
+          clientId="26362473693-3s6p4ahk26jsrmpeu2f0vgcg47krjm0t.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={(resp: GoogleLoginResponse) =>
+            this.saveUserID(resp.profileObj)
+          }
+          onFailure={this.responseGoogle}
+          cookiePolicy={"single_host_origin"}
+          scope="https://mail.google.com/"
+        />
+        ,
+      </>
+    );
+  }
 
+  private saveUserID = async (profileObj: any /* TODO: type for this*/) => {
+    const response = await fetch("api/auth", {
+      method: "POST",
+      body: JSON.stringify({ profileObj }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      console.log("success");
+    }
+  };
+
+  private getTime = async () => {
+    const response = await fetch("/api/test", { method: "GET" });
+    if (response.ok) {
+      this.setState({ time: await response.text() });
+    }
+  };
 }
 
 export function start() {
-    const rootElem = document.getElementById('main');
-    render(<App name="Hello World" />, rootElem);
+  const rootElem = document.getElementById("main");
+  render(<App name="Hello World" />, rootElem);
 }
